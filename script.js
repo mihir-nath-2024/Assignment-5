6e0d607b8d085e81700aef37561f6cffa58da604
mihir-nath-2024 <mihirnath114@gmail.com>
2024-09-23 23:07:43 +0600

    almost done

--- script.js
+++ script.js
@@ -9,3 +9,31 @@ document.getElementById('btn-history').addEventListener("click", function(){
     document.getElementById('history-section').style.display="block";
     document.getElementById('donate-section').style.display="none";
 })
+
+// card add money
+let noakhalifinalBalance = 0;
+document.getElementById('btn-noakhali').addEventListener("click", function(event){
+    event.preventDefault;
+    let noakhaliInputAmount = parseFloat(document.getElementById("input-noakhali").value);
+    let balanceNoakhali = document.getElementById('noakhali-Final')
+
+    // let noakhaliFinalAmount = document.getElementById('noakhali-Final').innerText + noakhaliInputAmount;
+
+    if(noakhaliInputAmount > 0  && !isNaN(noakhaliInputAmount)){
+        noakhalifinalBalance += noakhaliInputAmount;
+       balanceNoakhali.textContent= noakhalifinalBalance;
+       document.getElementById('my_modal_6').checked = true;
+               
+    }
+    else{
+        alert('Invalid Data');
+    }
+    
+        
+    
+
+   
+   
+
+})
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['index.html'],
+  content: ['./index.html',
+    './blog.html'
+  ],
   theme: {
     extend: {
       colors: {
